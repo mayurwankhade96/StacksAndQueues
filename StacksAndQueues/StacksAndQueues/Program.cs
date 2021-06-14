8516c80f2f0975441eae51ade1f351038e592cc9
Mayur Wankhade <mayur.wankhade2@gmail.com>
2021-06-14 11:33:51 +0530

    [Mayur] Add : Welcome message added...

---
+++ StacksAndQueues/StacksAndQueues/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StacksAndQueues
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Stacks and Queues Program!");
+        }
+    }
+}
