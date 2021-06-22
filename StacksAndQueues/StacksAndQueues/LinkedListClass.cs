e6ec6289c6bc12ffae5bf9ce89d4dcda6f51f3c5
Mayur Wankhade <mayur.wankhade2@gmail.com>
2021-06-23 00:44:26 +0530

    [Mayur] Add : Created a stack and pushed elements to it.

---
+++ StacksAndQueues/StacksAndQueues/LinkedListClass.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StacksAndQueues
+{
+    class LinkedListClass
+    {
+        public Node top;
+        public LinkedListClass()
+        {
+            this.top = null;
+        }
+
+        /// <summary>
+        /// Method to push element in the stack
+        /// </summary>
+        /// <param name="value"></param>
+        public void Push(int value)
+        {
+            Node node = new Node(value);
+            if (this.top == null)
+            {
+                node.next = null;
+            }
+            else
+            {
+                node.next = this.top;
+            }
+            this.top = node;
+            Console.WriteLine(value + " pushed to stack");
+        }
+
+        /// <summary>
+        /// Method to display stack
+        /// </summary>
+        public void Display()
+        {
+            Node temp = this.top;
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+}
--- StacksAndQueues/StacksAndQueues/Program.cs
+++ StacksAndQueues/StacksAndQueues/Program.cs
@@ -7,6 +7,13 @@ namespace StacksAndQueues
         static void Main()
         {
             Console.WriteLine("Welcome to Stacks and Queues Program!");
+
+            // Creating object
+            LinkedListClass SQ = new LinkedListClass();
+            SQ.Push(70);
+            SQ.Push(30);
+            SQ.Push(56);
+            SQ.Display();
         }
     }
 }
